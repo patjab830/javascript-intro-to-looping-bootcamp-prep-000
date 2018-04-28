39eab51ed4e973eff08498a562de5e56e375ab73
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 20:56:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,16 @@
 function forLoop(arr) {
   for ( int i = 1; i < 26; i++ ) {
     if ( i == 1 ) {
-      "I am 1 strange loop."
+      arr[i] = "I am 1 strange loop.";
     } else {
-      
+      arr[i] = `I am ${i} strange loops.`;
     }
   }
+}
+
+function whileLoop(num) {
+  while () {
+    
+  }
+  return 'done';
 }
